2cc8b6f9d1766e9ef6586d889d3c18a12d682287
ci_lynx <ci_lynx@users.noreply.github.com>
2025-12-25 23:10:41 +0800

    [Refactor][Android][Clay] Move Clay UI Render Creator to Lynx Clay
    Move clay related create logic to lynx clay to avoid reflect issues

--- platform/android/lynx_android/src/main/java/com/lynx/tasm/IUIRendererCreator.java
+++ platform/android/lynx_android/src/main/java/com/lynx/tasm/IUIRendererCreator.java
@@ -3,12 +3,30 @@
 // LICENSE file in the root directory of this source tree.
 package com.lynx.tasm;
 
+import com.lynx.tasm.behavior.ClayRenderMode;
 import com.lynx.tasm.behavior.ILynxUIRenderer;
 
 public interface IUIRendererCreator {
   /**
    * Provide the implementation of platform layer UI-related components/method
+   *
    * @return UIRender
    */
   ILynxUIRenderer createLynxUIRender();
+
+  public default void setLowEndImageTextureCacheMaxLimit(int limit) {}
+
+  public default void setImageTextureCacheMaxLimit(int limit) {}
+
+  public default void setUseTextureViewInClayMode(boolean useTextureView) {}
+
+  public default void setEnableGLFunctorInClayMode(boolean enable) {}
+
+  public default void setClayRenderMode(ClayRenderMode mode) {}
+
+  public default void setEnableDelegateInClayMode(boolean enable) {}
+
+  public default void setEnableClayRecycleEngine(boolean enable) {}
+
+  public default void setEnableClayCompatMode(boolean enable) {}
 }
